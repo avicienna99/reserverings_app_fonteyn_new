7d9b7727202a33f750a60fbe92d12d8f9e474090
nadim <343037@student.fontys.nl>
2024-12-30 15:01:40 +0100

    Added database connection and GitHub Actions workflow

--- main.go
+++ main.go
@@ -1,22 +1,74 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-var check string
+var db *sql.DB
 
 func init() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbname)
+
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	// Test the database connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	log.Println("Database connection successful")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "website test 123")
+	log.Printf("Request received: %s %s", r.Method, r.URL.Path)
+	fmt.Fprintln(w, "Website test 123")
 }
 
 func main() {
-	fmt.Println("test")
+
+	defer func() {
+		if db != nil {
+			err := db.Close()
+			if err != nil {
+				log.Printf("Error closing database connection: %v", err)
+			}
+		}
+	}()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+
+	// Start HTTP server
+	fmt.Printf("Starting server on port %s...\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
